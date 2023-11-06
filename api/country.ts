e96c798dbba856f94d42dacf5ce137dd35d37c3f
cramBamBulla <forbidden11315@gmail.com>
2023-11-07 00:44:40 +0100

    Remove .idea

--- api/country.ts
+++ api/country.ts
@@ -1,16 +1,16 @@
 import { baseApi } from '../app/query';
 
 export const countryApi = baseApi.injectEndpoints({
-  endpoints: build => ({
-    getCountries: build.query<any, void>({
-      query: () => {
-        return {
-          method: 'get',
-          url: '/country/full',
-        };
-      },
-    }),
-  }),
+	endpoints: build => ({
+		getCountries: build.query<any, void>({
+			query: () => {
+				return {
+					method: 'get',
+					url: '/country/full',
+				};
+			},
+		}),
+	}),
 });
 
 export const { useGetCountriesQuery } = countryApi;
