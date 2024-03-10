a54953e1ce47b94cddeb83c6d3509c99e6bc09c2
cramBamBulla <forbidden11315@gmail.com>
2024-03-10 12:20:41 +0100

    image cache template

---
+++ utils/cache.ts
@@ -0,0 +1,56 @@
+import * as FileSystem from 'expo-file-system';
+
+const getFileNameFromUri = (uri: string) => {
+  return uri.split('/').pop();
+};
+
+const getCacheDirectoryForUri = (uri: string) => {
+  return `${FileSystem.cacheDirectory}${getFileNameFromUri(uri)}`;
+};
+
+export async function findImageInCache(uri) {
+  try {
+    const cacheUri = getCacheDirectoryForUri(uri);
+    let info = await FileSystem.getInfoAsync(cacheUri);
+    return { ...info, err: false };
+  } catch (error) {
+    return {
+      exists: false,
+      err: true,
+      msg: error,
+      uri: '',
+    };
+  }
+}
+
+export async function cacheImage(uri) {
+  try {
+    const cacheUri = getCacheDirectoryForUri(uri);
+    const downloadImage = FileSystem.createDownloadResumable(uri, cacheUri, {});
+    const downloaded = await downloadImage.downloadAsync();
+    console.log({ downloaded });
+    return {
+      cached: true,
+      err: false,
+      path: downloaded?.uri,
+    };
+  } catch (error) {
+    return {
+      cached: false,
+      err: true,
+      msg: error,
+    };
+  }
+}
+
+export const cacheBunchImages = async (imagesUris: string[]) => {
+  await Promise.all(
+    imagesUris.map(async uri => {
+      const imageInCache = await findImageInCache(uri);
+
+      if (!imageInCache.exists || !imageInCache.uri) {
+        await cacheImage(uri);
+      }
+    })
+  );
+};
