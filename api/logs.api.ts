75ed8c6d8bb961e5dff79e3e650115d63987ab06
alexejbronshtein <wertyga@github.com>
2024-10-07 19:07:26 +0200

    added aviasales

--- api/logs.api.ts
+++ api/logs.api.ts
@@ -1,6 +1,10 @@
 import { baseQuery } from '@/app/query';
 
+import { getIsNetConnected } from '@/utils/etc';
+
 export const sendLogs = async (data: any): Promise<void> => {
+  if (!getIsNetConnected()) return;
+
   try {
     await baseQuery({
       method: 'post',
